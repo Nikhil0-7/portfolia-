92907abf4924c45be691b585ee8fdecb63c0fc4f
Nikhil Kumar <nikhilsingh86511@gmail.com>
2024-05-07 20:43:34 +0530

    add resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile from '../../assets/profile.png'
+import resume from '../../assets/NikhilKumar Resume.pdf'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
@@ -11,7 +12,7 @@ const Hero = () => {
         <p>I am a fresher and looking for an opportunity where I can show my skills </p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My Resume</div>
+            <div className="hero-resume"><a href={resume}>My Resume</a></div>
         </div>
 
     </div>
